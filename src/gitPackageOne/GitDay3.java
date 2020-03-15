39c48d454417d420125a8bb0ae78fe21e05f00e8
Mustafa Erdogan <erakmuh@gmail.com>
2020-03-15 21:51:44 +0100

    fourth push from local branch

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -8,6 +8,7 @@ public class GitDay3 {
 		System.out.println("this is my feature branch");
 		System.out.println("my second push from local feature branch");
 		System.out.println("my third push from local feature branch");
+		System.out.println("my fourth push from local feature branch");
 
 	}
 }
